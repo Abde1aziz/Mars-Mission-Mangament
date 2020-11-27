98fa255f0b28bceefe414a7d42484fda992c5355
Abdelaziz Rashed <abdelaziz.y.rashed@gmail.com>
2020-11-27 14:00:06 +0200

    itialize projct

---
+++ Mars Mission Mangament/Mars Mission Mangament.cpp
@@ -0,0 +1,21 @@
+// Mars Mission Mangament.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int currentDay = 0;
+    int mode;
+    bool exit = false;
+    cout << "Choose the program running mode.\nWhere....:\n1- Interactive mode\n2-Step by Step mode\n3- Silent Mode" << endl;
+    cin >> mode;
+    while (!exit) {
+
+    }
+}
+
